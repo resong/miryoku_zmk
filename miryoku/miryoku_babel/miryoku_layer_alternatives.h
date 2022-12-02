9bb23b13e1973f5b9efa1d9c766d20f39f8321b3
Rebecca Song <rebecca.song@outlook.com>
2022-12-01 22:29:21 -0500

    Apply gqt to home row mods

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -116,9 +116,9 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
+GQT(LGUI, A),      GQT(LALT, S),     GQT(LCTRL, D),    GQT(LSHFT, F),    &kp G,             &kp H,             GQT(LSHFT, J),    GQT(LCTRL, K),    GQT(LALT, L),     GQT(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, RET),  GQT(U_MOUSE, BSPC),GQT(U_SYM, DEL),  U_LT(U_NUM, SPC), U_LT(U_FUN, TAB),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, RET),  U_LT(U_MOUSE, BSPC),U_LT(U_SYM, DEL),  U_LT(U_NUM, SPC), U_LT(U_FUN, TAB),  U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTZ \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,             &kp U,             &kp I,             &kp O,             &kp P,             \
